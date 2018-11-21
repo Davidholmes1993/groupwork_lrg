14ebd7542863e735f117e35c9f86c47a5fbc4839
David Holmes <davidholmes1993@gmail.com>
2018-11-21 19:15:28 +0000

    Cleared empty lines

--- gene_name_function.py
+++ gene_name_function.py
@@ -1,10 +1,11 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('LRG_1.xml')
+tree = ET.parse('LRG_384.xml')
 root = tree.getroot()
 
 
 name=root[0][4][0].text
 list=name.split()
 print (list[-1])
+
 
 
