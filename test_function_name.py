28fcc917f5be081e274a64c00747faf2b4116a98
Maria Lock <maria.lock@postgrad.manchester.ac.uk>
2018-11-22 10:31:07 +0000

    Added argparse module to allow user input of file

--- test_function_name.py
+++ test_function_name.py
@@ -1,7 +1,20 @@
+# import etree module
 import xml.etree.ElementTree as ET
-tree = ET.parse('LRG_1.xml')
-root = tree.getroot()
 
-#Searches the whole xml file for the root called lrg_locus and prints the text in that line, which is the HGNC gene name
-for lrg_locus in root.iter('lrg_locus'):
-      print (lrg_locus.text)
+# import argparse. This will let user define file and open this file as 'file'
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+with open(args.filename) as file:
+    """
+    Searches the whole xml file for the root called lrg_locus and prints the text in that 
+    line, which is the HGNC gene name
+    """
+    tree = ET.parse(file)
+    root = tree.getroot()
+    for lrg_locus in root.iter('lrg_locus'):
+        print (lrg_locus.text)
+
+
