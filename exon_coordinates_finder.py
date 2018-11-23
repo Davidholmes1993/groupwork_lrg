b9d85d5d0e744d0c20ce5eb41a4b34c1f014fe7d
Maria Lock <maria.lock@postgrad.manchester.ac.uk>
2018-11-23 10:42:40 +0000

    Added comment for argparse file opener and tidied other comments

--- exon_coordinates_finder.py
+++ exon_coordinates_finder.py
@@ -2,8 +2,9 @@
 import xml.etree.ElementTree as ET
 
 # import argparse. This will let user define file and open this file as 'file'
-# This defines the filename that the user inputted
 import argparse
+
+# This defines the filename that the user inputted
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
 args = parser.parse_args()
@@ -18,27 +19,27 @@ else:
     print("ERROR: Invalid file type. File must have extension .xml")
     exit()
 
+# This uses argparse to open the file that the user has inputted
 with open(args.filename) as file:
 
-
-#This finds the HGNC name for the gene
+# This finds the HGNC name for the gene
     tree = ET.parse(file)
     root = tree.getroot()
     for lrg_locus in root.iter('lrg_locus'):
         gene = lrg_locus.text
 
 
-#This specifies the default gene transcript in the transcript_name=id
-#And creates a .bed file by using the gene name as the prefix
+# This specifies the default gene transcript in the transcript_name=id
+# And creates a .bed file by using the gene name as the prefix
 f = open("%s.bed" % (gene),"w+")
 f.write("Gene name:" + gene + "\n")
 for id in root.iter('id'):
     transcript_name= id.text
 
-#This loop identifies the LRG number as coord_system,
-#The exon number is identified as the label
-#The coordinates for each exon are identified
-#As well as the start and end coordinates for each exon
+# This loop identifies the LRG number as coord_system,
+# The exon number is identified as the label
+# The coordinates for each exon are identified
+# As well as the start and end coordinates for each exon
 for exon in root.findall('.//fixed_annotation/transcript/exon'):
     label = exon.get('label')
     coordinates = exon.find('coordinates').attrib
@@ -48,8 +49,8 @@ for exon in root.findall('.//fixed_annotation/transcript/exon'):
     if coord_system == transcript_name:
         f.write(coord_system + " Exon: " + label + " Start: " + start + " End: " + end + "\n")
 
-#The .bed file needs to be closed after creating it
+# The .bed file needs to be closed after creating it
 f.close()
 
-#A message is created to show the user where to find their results
+# A message is created to show the user where to find their results
 print("Your results are found in the %s.bed file" % (gene))
