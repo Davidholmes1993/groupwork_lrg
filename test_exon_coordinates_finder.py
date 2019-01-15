e1efa49d1bf4e5415a279c08c9b6fd4c02919600
dholmes14 <David.holmes14@nhs.net>
2019-01-15 15:44:48 +0000

    added assertion error messages to the pytest

--- test_exon_coordinates_finder.py
+++ test_exon_coordinates_finder.py
@@ -17,8 +17,10 @@ COL1A1 as an answer
 def test_find_gene():
 	tree=ET.parse('LRG_1.xml')
 	root = tree.getroot()
-	assert find_gene(root) == 'COL1A1'
-
+	try:
+		assert find_gene(root) == 'COL1A1'
+	except AssertionError:
+		raise( AssertionError("The gene name does not match the expected gene name"))
 """
 This function will test that when given the file LRG_1.xml the get_lrg_number function gives
 LRG_1 as an answer
@@ -26,10 +28,12 @@ LRG_1 as an answer
 def test_get_lrg_number():
 	tree=ET.parse('LRG_1.xml')
 	root = tree.getroot()
-	assert get_lrg_number(root) == 'LRG_1'
-
+	try:
+		assert get_lrg_number(root) == 'LRG_1'
+	except AssertionError:
+		raise( AssertionError("The LRG number does not match the expected LRG number"))
 """
-This function will test that when given the file LRG_1.xml and chosen genomic build 37 the 
+This function will test that when given the file LRG_1.xml and chosen genomic build 37 the
 find_genomic_coord function gives strand = -1, chromosome_number = 17, genomic_start = 48259457
 and genomic_end = 48284000
 """
@@ -37,10 +41,12 @@ def test_GRCh37_find_genomic_coord():
 	tree=ET.parse('LRG_1.xml')
 	root = tree.getroot()
 	build = '37'
-	assert find_genomic_coord(root, build) == ('-1', '17', 48259457, 48284000)
-
+	try:
+		assert find_genomic_coord(root, build) == ('-1', '17', 48259457, 48284000)
+	except AssertionError:
+		raise( AssertionError("The chromosome, genomic coordinates, and strand do not match with those expected"))
 """
-This function will test that when given the file LRG_1.xml and chosen genomic build 38 the 
+This function will test that when given the file LRG_1.xml and chosen genomic build 38 the
 find_genomic_coord function gives strand = -1, chromosome_number = 17, genomic_start = 50182096
 and genomic_end = 50206639
 """
@@ -48,4 +54,7 @@ def test_GRCh38_find_genomic_coord():
 	tree=ET.parse('LRG_1.xml')
 	root = tree.getroot()
 	build = '38'
-	assert find_genomic_coord(root, build) == ('-1', '17', 50182096, 50206639)
+	try:
+		assert find_genomic_coord(root, build) == ('-1', '17', 50182096, 50206639)
+	except AssertionError:
+		raise( AssertionError("The chromosome, genomic coordinates, and strand do not match with those expected"))
